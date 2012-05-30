015bc94ef8b90c31950dfdbf465973e75328cba3
Niek van der Kooy <niek@vanderkooy.info>
2012-05-30 18:05:22 +0200

    Prevent using app without valid credentials

--- src/info/vanderkooy/ucheck/Account.java
+++ src/info/vanderkooy/ucheck/Account.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +17,7 @@ public class Account extends Activity {
 	private Preferences prefs;
 	private EditText username;
 	private EditText password;
+	private CheckBox storePass;
 	private ProgressDialog dialog;
 	
 
@@ -30,6 +33,7 @@ public class Account extends Activity {
 		Button loginButton = (Button) findViewById(R.id.login);
 		username = (EditText) findViewById(R.id.username);
 		password = (EditText) findViewById(R.id.password);
+		storePass = (CheckBox) findViewById(R.id.remember);
 
 		dialog = ProgressDialog.show(Account.this, "", "", true);
 		dialog.show();
@@ -38,6 +42,7 @@ public class Account extends Activity {
 			loginButton.setText("Uitloggen");
 			username.setText(prefs.getUsername());
 			password.setText("........");
+			storePass.setChecked(prefs.getStorePass());
 			loginButton.setOnClickListener(logoutListener);
 		} else {
 			loginButton.setText("Inloggen");
@@ -68,10 +73,11 @@ public class Account extends Activity {
 	private OnClickListener loginListener = new OnClickListener() {
 		public void onClick(View v) {
 			String usernameString = username.getText().toString();
-			if(!(usernameString.substring(0, 1) == "s"))
+			if(!usernameString.substring(0, 1).equals("s"))
 				usernameString = "s" + usernameString;
 			prefs.setUsername(usernameString);
 			prefs.setPassword(password.getText().toString());
+			prefs.setStorePass(storePass.isChecked());
 			dialog.show();
 			boolean success = handler.verifyLogin();
 			dialog.hide();
