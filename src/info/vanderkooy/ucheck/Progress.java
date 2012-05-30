75107e9ecccc39dca6f64661e4c5513bed64d4b4
Niek van der Kooy <niek@vanderkooy.info>
2012-05-30 21:20:08 +0200

    Implemented WebView with progress page

--- src/info/vanderkooy/ucheck/Progress.java
+++ src/info/vanderkooy/ucheck/Progress.java
@@ -2,14 +2,26 @@ package info.vanderkooy.ucheck;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class Progress extends Activity {
-
+	private WebView webView;
+	private APIHandler handler;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.progress);
+	    webView = (WebView) findViewById(R.id.webView);
+	    handler = new APIHandler(getApplicationContext());
+	    String progressData = handler.getProgress();
+	    if(progressData.equals("")) {
+	    	Toast toast = Toast.makeText(getApplicationContext(), "Er is iets mis gegaan bij het ophalen van voortgangsdata. Probeer het later nog een keer.", 6);
+	    	toast.show();
+	    }
+	    webView.loadData(progressData, "text/html", null);
 	}
 
 }
