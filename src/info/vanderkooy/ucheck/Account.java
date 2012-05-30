54de61c8ab7f16fd77723ea61bf98ef2ffaa8452
Niek van der Kooy <niek@vanderkooy.info>
2012-05-30 16:17:38 +0200

    Added info page

--- src/info/vanderkooy/ucheck/Account.java
+++ src/info/vanderkooy/ucheck/Account.java
@@ -1,16 +1,29 @@
 package info.vanderkooy.ucheck;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Account extends Activity {
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.account);
-	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.account);
+
+		Button infoButton = (Button) findViewById(R.id.info);
+		infoButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent infoIntent = new Intent().setClass(Account.this,
+						Info.class);
+				Account.this.startActivity(infoIntent);
+			}
+		});
+
 	}
 
 }
---
+++ src/info/vanderkooy/ucheck/Info.java
@@ -0,0 +1,16 @@
+package info.vanderkooy.ucheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.info);
+	    // TODO Auto-generated method stub
+	}
+
+}
