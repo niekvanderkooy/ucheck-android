4dcb8c8feb3fc36e5b8966abe0e84c4e2f5a277f
Niek van der Kooy <niek@vanderkooy.info>
2012-06-28 23:37:18 +0200

    Threaded login (Don't forget to dismiss dialogs ;) )

--- src/info/vanderkooy/ucheck/Login.java
+++ src/info/vanderkooy/ucheck/Login.java
@@ -1,9 +1,9 @@
 package info.vanderkooy.ucheck;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,8 +21,9 @@ public class Login extends Activity {
 	private String usr;
 	private Button infoButton;
 	private Button loginButton;
-//	private Button newData;
-	
+	private ProgressDialog dialog;
+
+	// private Button newData;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -32,58 +33,90 @@ public class Login extends Activity {
 
 		handler = new APIHandler(getApplicationContext());
 		prefs = new Preferences(getApplicationContext());
-		
+
 		infoButton = (Button) findViewById(R.id.info);
 		loginButton = (Button) findViewById(R.id.login);
 		username = (EditText) findViewById(R.id.username);
 		password = (EditText) findViewById(R.id.password);
 		storePass = (CheckBox) findViewById(R.id.remember);
-		
-//		newData.setVisibility(8);
-		
+
+		// newData.setVisibility(8);
+
 		usr = prefs.getUsername();
-		if(usr == null)
+		if (usr == null)
 			username.setHint(getString(R.string.studentNumberHint));
 		else
 			username.setText(usr);
 		password.setHint(getString(R.string.passHint));
 		password.setTextSize(13);
 		storePass.setChecked(prefs.getStorePass());
-		
+
 		loginButton.setText(getString(R.string.login));
 		loginButton.setOnClickListener(loginListener);
 		infoButton.setOnClickListener(infoButtonListener);
 	}
-	
+
 	private OnClickListener loginListener = new OnClickListener() {
 		public void onClick(View v) {
 			String usernameString = username.getText().toString();
-			if(usernameString.length() >= 1 && !usernameString.substring(0, 1).equals("s"))
+
+			if (usernameString.length() >= 1
+					&& !usernameString.substring(0, 1).equals("s"))
 				usernameString = "s" + usernameString;
-			int returned = 0;
-			boolean success = (usernameString.length() < 7 || usernameString.length() > 9) ? false : ((returned = handler.getKey(usernameString, password.getText().toString())) == 1 ? true : false);
-			if(success) {
-				prefs.setStorePass(storePass.isChecked());
-				if(!usr.equals(usernameString))
-					prefs.forceNewData();
-				finish();
-			} else {
-				Toast toast;
-				if(returned == 0) {
-					toast = Toast.makeText(getApplicationContext(), getString(R.string.userError), 3);
-				} else {
-					toast = Toast.makeText(getApplicationContext(), getString(R.string.verificationError), 10);
+
+			dialog = ProgressDialog.show(Login.this, "",
+					getString(R.string.login_action), true);
+
+			final String finalUsernameString = usernameString;
+			Thread thread = new Thread(new Runnable() {
+				public void run() {
+					int returned = 0;
+					final boolean success = (finalUsernameString.length() < 7 || finalUsernameString
+							.length() > 9) ? false : ((returned = handler
+							.getKey(finalUsernameString, password.getText()
+									.toString())) == 1 ? true : false);
+					final int finalReturned = returned;
+
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							if (success) {
+								prefs.setStorePass(storePass.isChecked());
+								// if(!usr.equals(finalUsernameString))
+								prefs.forceNewData();
+								dialog.hide();
+								dialog.dismiss();
+								finish();
+							} else {
+								Toast toast;
+								if (finalReturned == 0) {
+									toast = Toast.makeText(
+											getApplicationContext(),
+											getString(R.string.userError), 3);
+								} else {
+									toast = Toast
+											.makeText(
+													getApplicationContext(),
+													getString(R.string.verificationError),
+													10);
+								}
+								toast.show();
+								dialog.hide();
+								dialog.dismiss();
+							}
+						}
+					});
 				}
-				toast.show();
-			}
+			});
+			thread.start();
+
 		}
 	};
-	
+
 	private OnClickListener infoButtonListener = new OnClickListener() {
 		public void onClick(View v) {
 			prefs.setGoingToInfo(true);
-			Intent infoIntent = new Intent().setClass(Login.this,
-					Info.class);
+			Intent infoIntent = new Intent().setClass(Login.this, Info.class);
 			Login.this.startActivity(infoIntent);
 		}
 	};
